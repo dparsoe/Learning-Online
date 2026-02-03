c1296fd66f0e6648fb24b7beb8dfd692b294aec0
Dpar <dparsoe.dps@gmail.com>
2026-02-03 20:26:04 +0630

    moduleES6start

---
+++ a.js
@@ -0,0 +1,28 @@
+const pi = 3.14;
+
+// function add(a,b){
+//     return a + b ;
+// }
+
+// function sub(a,b){
+//     return a - b;
+
+// }
+
+
+//CommonJS Module (CJS)
+// module.exports = {pi,add,sub};
+
+
+
+//ES6 Module (ESM)ရေးနည်း
+export const num = 3.14;
+
+export function add(a,b){
+    return a + b ;
+}
+
+export function sub(a,b){
+    return a - b;
+
+}
---
+++ b.js
@@ -0,0 +1,12 @@
+// let a = require("./a.js");
+
+// console.log(a.pi);
+// console.log(a.add(1,2));
+// console.log(a.sub(5,2));
+
+
+//ES6 Module (ESM)ရေးနည်း
+import {num,add,sub} from "./a.js";
+console.log(num);
+console.log(add(1,2));
+console.log(sub(5,2));
